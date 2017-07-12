095326b7210ad23a41c3094f0fa5786009cafaf4
PRASHANT KUMAR <prashantsrivastava1994@outlook.com>
2017-07-12 10:57:37 +0530

    Controller modified to handle empty array and change events in variables

--- SliderController.js
+++ SliderController.js
@@ -1,79 +1,94 @@
 ({
     doInit : function(component, event, helper){
-         //setting global id
+        //setting global id
         var g_id = component.getGlobalId();
         var myGlobalClass ='_'+ g_id.replace(/:/g,'_').replace(/;/g,'_')+'specialClass';
         component.set('v.myGlobalClass', myGlobalClass);
         
-    	helper.handleAnimTypeChange(component);
+        if(component.get('v.imageArray').length == 0){
+            console.log('Image Array has length 0, i.e. No limage links found!');
+        }
+        else{
+            helper.initializationHelper(component);
+        }
         
-        var defaultDirection = component.get('v.defaultDirection');
-        var direction = component.get('v.'+defaultDirection);
-        
-        var current = component.get("v.current");
-        var duration = component.get('v.duration');
-        var autoMove = component.get('v.autoMove');
-        
-        helper.handleImageArrayChange(component);
-        if(autoMove)
-        {
-            setTimeout(function(){
-                if(current == 0) {
-                    helper.moveTo(component, 1, direction);
+    },
+    moveToPosition : function(component, event, helper) {
+        if(component.get('v.imageArray').length == 0){
+            console.log('Image Array has length 0, i.e. No limage links found!');
+        }
+        else{
+            var clickedIndex = +(event.target.id);
+            var current = component.get("v.current");
+            
+            var prev = component.get('v.prev');
+            var next = component.get('v.next');
+            
+            if(clickedIndex == current) {
+                //do nothing
+            }
+            else if(clickedIndex < current) {
+                helper.moveTo(component, clickedIndex, prev);
+            }
+                else {
+                    helper.moveTo(component, clickedIndex, next);
                 }
-            }, duration);
         }
         
         
     },
-	moveToPosition : function(component, event, helper) {
-        var clickedIndex = +(event.target.id);
-        var current = component.get("v.current");
-        
-        var prev = component.get('v.prev');
-        var next = component.get('v.next');
-        
-        if(clickedIndex == current) {
-            //do nothing
-        }
-        else if(clickedIndex < current) {
-             helper.moveTo(component, clickedIndex, prev);
-        }
-        else {
-            helper.moveTo(component, clickedIndex, next);
-        }
-       
-	},
     moveToPrev : function(component, event, helper) {
-       var current = component.get("v.current");
-       var circular = component.get("v.circular");
-       var prev = component.get('v.prev');
-       
-       if(current == 0) {
-           if(circular == "true") {
-               var arrayLength = component.get('v.imageArraySupport').length;
-               helper.moveTo(component, arrayLength - 1, prev);
-           }
-       }
-       else {
-         	helper.moveTo(component, current - 1, prev);   
+        if(component.get('v.imageArray').length == 0){
+            console.log('Image Array has length 0, i.e. No limage links found!');
         }
-	},
-    moveToNext : function(component, event, helper) {
-       var arrayLength = component.get('v.imageArraySupport').length;
-       var current = component.get("v.current");
-       var circular = component.get("v.circular");
-       var next = component.get('v.next');
+        else{
+            var current = component.get("v.current");
+            var circular = component.get("v.circular");
+            var prev = component.get('v.prev');
+            
+            if(current == 0) {
+                if(circular == true) {
+                    var arrayLength = component.get('v.imageArraySupport').length;
+                    helper.moveTo(component, arrayLength - 1, prev);
+                }
+            }
+            else {
+                helper.moveTo(component, current - 1, prev);   
+            }
+        }
         
-       if(current == (arrayLength - 1)) {
-           if(circular == "true") {
-               var arrayLength = component.get('v.imageArraySupport').length;
-               helper.moveTo(component, 0, next);
-           }
-       }
-       else {
-         	helper.moveTo(component, current + 1, next);   
+    },
+    moveToNext : function(component, event, helper) {
+        if(component.get('v.imageArray').length == 0){
+            console.log('Image Array has length 0, i.e. No limage links found!');
         }
-	}
+        else{
+            
+            var arrayLength = component.get('v.imageArraySupport').length;
+            var current = component.get("v.current");
+            var circular = component.get("v.circular");
+            var next = component.get('v.next');
+            
+            if(current == (arrayLength - 1)) {
+                if(circular == true) {
+                    var arrayLength = component.get('v.imageArraySupport').length;
+                    helper.moveTo(component, 0, next);
+                }
+            }
+            else {
+                helper.moveTo(component, current + 1, next);   
+            }
+        }
+        
+    },
+    handleChange : function(component, event, helper) {
+        if(component.get('v.imageArray').length == 0){
+            console.log('Image Array has length 0, i.e. No limage links found!');
+        }
+        else{
+            helper.initializationHelper(component);
+        }
+        
+    }
     
 })
