47a6e3307c5153f5be0400c06ae5d1218b977c4b
Prashant Kumar <prashantsrivastava1994@outlook.com>
2022-07-23 14:26:00 +1000

    Repository updated to support deploy to salesforce Changes: Added support to configure this component using design attributes

---
+++ src/aura/Slider/SliderController.js
@@ -0,0 +1,143 @@
+({
+    doInit : function(component, event, helper){
+        //setting global id
+        var g_id = component.getGlobalId();
+        var myGlobalClass ='_'+ g_id.replace(/:/g,'_').replace(/;/g,'_')+'specialClass';
+        component.set('v.myGlobalClass', myGlobalClass);
+        
+        try{
+            if(component.get('v.imageArray').length == 0){
+                console.log('Image Array has length 0, i.e. No limage links found!');
+                
+                if((component.get('v.imageArrayStr') || '').trim()) {
+                	helper.initializeFromDesignAttribute(component);
+                }
+            }
+            helper.initializationHelper(component);
+		}
+        catch (ex) {
+            console.error(ex);
+        }
+        
+        
+    },
+    moveToPosition : function(component, event, helper) {
+        if(component.get('v.imageArray').length == 0){
+            console.log('Image Array has length 0, i.e. No limage links found!');
+        }
+        else{
+            var clickedIndex = +(event.target.id);
+            var current = component.get("v.current");
+            
+            var prev = component.get('v.prev');
+            var next = component.get('v.next');
+            
+            if(clickedIndex == current) {
+                //do nothing
+            }
+            else if(clickedIndex < current) {
+                helper.moveTo(component, clickedIndex, prev);
+            }
+                else {
+                    helper.moveTo(component, clickedIndex, next);
+                }
+        }
+        
+        
+    },
+    moveToPrev : function(component, event, helper) {
+        if(component.get('v.imageArray').length == 0){
+            console.log('Image Array has length 0, i.e. No limage links found!');
+        }
+        else{
+            var current = component.get("v.current");
+            var circular = component.get("v.circular");
+            var prev = component.get('v.prev');
+            
+            if(current == 0) {
+                if(circular == true) {
+                    var arrayLength = component.get('v.imageArraySupport').length;
+                    helper.moveTo(component, arrayLength - 1, prev);
+                }
+            }
+            else {
+                helper.moveTo(component, current - 1, prev);   
+            }
+        }
+        
+    },
+    moveToNext : function(component, event, helper) {
+        if(component.get('v.imageArray').length == 0){
+            console.log('Image Array has length 0, i.e. No limage links found!');
+        }
+        else{
+            
+            var arrayLength = component.get('v.imageArraySupport').length;
+            var current = component.get("v.current");
+            var circular = component.get("v.circular");
+            var next = component.get('v.next');
+            
+            if(current == (arrayLength - 1)) {
+                if(circular == true) {
+                    var arrayLength = component.get('v.imageArraySupport').length;
+                    helper.moveTo(component, 0, next);
+                }
+            }
+            else {
+                helper.moveTo(component, current + 1, next);   
+            }
+        }
+        
+    },
+    handleChange : function(component, event, helper) {
+        if(component.get('v.imageArray').length == 0){
+            console.log('Image Array has length 0, i.e. No limage links found!');
+        }
+        else{
+            helper.initializationHelper(component);
+        }
+        
+    },
+    touchstart : function(component, event, helper) {
+        helper.swipeObject.start.x = event.touches[0].pageX;
+        helper.swipeObject.start.y = event.touches[0].pageY;
+    },
+    touchend : function(component, event, helper) {
+        var obj = helper.swipeObject;
+        var navigation = component.get('v.navigation');
+        
+        if(navigation == 'horizontal'){
+            if((obj.end.x - obj.start.x)>0){
+                component.movePrev(); 
+            }
+            else if((obj.end.x - obj.start.x)<0){
+                component.moveNext();
+            }
+        }
+        if(navigation == 'vertical'){
+            if((obj.end.y - obj.start.y)>0){
+                component.moveNext();
+            }
+            else if((obj.end.y - obj.start.y)<0){
+                component.movePrev();
+            }
+        }
+        
+        helper.swipeObject.start.x = null;
+        helper.swipeObject.start.y = null;
+        helper.swipeObject.end.x = null;
+        helper.swipeObject.end.y = null;
+    },
+    touchmove : function(component, event, helper) {
+        helper.swipeObject.end.x = event.touches[0].pageX;
+        helper.swipeObject.end.y = event.touches[0].pageY;
+    },
+    touchcancel : function(component, event, helper) {
+        helper.swipeObject.start.x = null;
+        helper.swipeObject.start.y = null;
+        helper.swipeObject.end.x = null;
+        helper.swipeObject.end.y = null;
+        
+    }
+    
+})
---
+++ src/aura/Slider/SliderHelper.js
@@ -0,0 +1,143 @@
+({
+    swipeObject : {
+        start : {x : null, y : null},
+        end : {x : null, y : null}
+    },
+    intervalRef: null,
+    initializationHelper : function(component) {
+        	var helper = this;
+        	helper.handleAnimTypeChange(component);
+            var defaultDirection = component.get('v.defaultDirection');
+        	
+            var direction = component.get('v.'+defaultDirection);
+        	
+            component.set("v.current", 0);
+        	var current = component.get("v.current");
+            var duration = component.get('v.duration');
+            var autoMove = component.get('v.autoMove');
+            
+            helper.handleImageArrayChange(component);
+        	
+        	if(helper.intervalRef)
+            	clearInterval(helper.intervalRef);
+            
+        	if(autoMove)
+            {
+                setTimeout(function(){
+                    if(current == 0) {
+                        helper.moveTo(component, 1, direction);
+                    }
+                }, duration);
+            }
+            
+
+    },
+	moveTo : function(component, moveToPos, direction) {
+        var helper = this;
+		var autoMove = component.get('v.autoMove');
+        var imageArraySupport = component.get('v.imageArraySupport');
+        var current = component.get('v.current');
+        
+        if(helper.intervalRef)
+            clearInterval(helper.intervalRef);
+        
+      	helper.doMoveRelatedTask(component, imageArraySupport, current, moveToPos, direction);
+        
+        if(autoMove) {
+            var duration = component.get('v.duration');
+            var defaultDirection = component.get('v.defaultDirection');
+        	var direction = component.get('v.'+defaultDirection);
+            
+            helper.intervalRef = setInterval(function(){
+                if(moveToPos == imageArraySupport.length) {
+                    var circular = component.get('v.circular');
+                    if(circular)
+                        moveToPos = 0;
+                    else
+                       clearInterval(helper.intervalRef); 
+                }
+                helper.doMoveRelatedTask(component, imageArraySupport, current, moveToPos, direction);
+                current = moveToPos;
+                moveToPos++;
+            }, duration);
+        }
+       
+	},
+    doMoveRelatedTask : function(component, imageArraySupport, current, moveToPos, direction){
+       
+        imageArraySupport[current].active = false;
+        imageArraySupport[moveToPos].active = true;
+        
+        for(var i in imageArraySupport){
+            imageArraySupport[i].direction = 'none';
+        }
+        imageArraySupport[current].direction = direction+'-end';
+        imageArraySupport[moveToPos].direction = direction;
+        component.set('v.current', moveToPos);
+        component.set("v.imageArraySupport", imageArraySupport);
+    },
+    handleImageArrayChange : function(component){
+    	var imageArray = component.get('v.imageArray');
+        var imageArraySupport = component.get('v.imageArraySupport');
+        
+        var showCaption = component.get('v.showCaption');
+       
+        for(var i in imageArray)
+        {	var obj = {
+            	src: imageArray[i].src,
+            	direction: 'none',
+            	active: false,
+            	caption: ''
+        	}
+        	if(showCaption)
+            	obj.caption = imageArray[i].caption;
+            imageArraySupport[i] = obj;
+        }
+        imageArraySupport[0].direction = 'initial';
+        imageArraySupport[0].active = true;
+        component.set('v.imageArraySupport', imageArraySupport);
+        
+	},
+    handleAnimTypeChange : function(component){
+        var animType = component.get('v.animType');
+        var prev,next;
+        if(animType == "horizontal-slide") {
+            prev = "right";
+            next = "left" ;
+        }
+        else if(animType == "vertical-slide") {
+            prev = "top";
+            next = "bottom" ;
+        }
+        else if(animType == "fade") {
+            prev = "fade";
+            next = "fade";
+        }
+        else if(animType == "popup")
+        {
+            prev = "popup";
+            next = "popup";
+        }
+        else if(animType == "merge")
+        {
+            prev = "merge";
+            next = "merge";
+        }
+        component.set('v.prev', prev);
+        component.set('v.next', next);
+    },
+    
+    initializeFromDesignAttribute: function(component) {
+        var imageArrayStr = component.get('v.imageArrayStr') || '';
+        imageArrayStr = imageArrayStr.replaceAll("'", "\"");
+        var imageArray = JSON.parse(imageArrayStr);
+        component.set('v.imageArray', imageArray);
+        
+        var height = component.get('v.height') || {};
+        height.large = component.get('v.heightInLargeDevice');
+        height.medium = component.get('v.heightInMediumDevice');
+        height.small = component.get('v.heightInSmallDevice');
+        
+        component.set('v.height', height);
+    }
+})
